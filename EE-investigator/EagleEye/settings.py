d9d33b6414c74f0bf79865270b889929bb75e5ea
Wouter Lauret <wlauret@een.com>
2022-09-19 10:25:34 +0200

    Replaced dummy API keys with * to make clear it's just a dummy API and you can not use it. You need to add your own API key

--- EE-investigator/EagleEye/settings.py
+++ EE-investigator/EagleEye/settings.py
@@ -4,4 +4,4 @@
 #  Get one at: https://apidocs.eagleeyenetworks.com/apidocs/#get-an-api-key
 ####
 
-api_key = '413753c2-30af-11ea-8ab3-a2d2184fcf8d'
+api_key = '****-****-****-****-****'
--- EE-manufacturing-detection/EagleEye/settings.py
+++ EE-manufacturing-detection/EagleEye/settings.py
@@ -4,4 +4,4 @@
 #  Get one at: https://apidocs.eagleeyenetworks.com/apidocs/#get-an-api-key
 ####
 
-api_key = 'c41e3006-02f4-11e9-8e18-0a580af400ae'
+api_key = '****-****-****-****-****'
